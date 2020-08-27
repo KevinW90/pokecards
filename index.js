c20910b51e6505c1390a43d1f1d175142a8ad584
KevinW90 <kevinjwilliams76@gmail.com>
2020-08-27 12:53:56 -0400

    basic plan of data

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+Use the pokemon api to search for pokemon and display a card element
+
+pokemon api root: https://pokeapi.co/api/v2
+pokemon endpoint: /pokemon/{id or name}
+  id .id#
+  name .name''
+  sprite img .sprites{}
+  type .types[]
+  ?description
+
+form
+  one input [name or id]
+  submit button
+
+card
+  img
+  name
+  ?description
+  type
+*/
