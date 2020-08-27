484e230a63fdfae1aa7c844bd67a00fbdbe739d2
KevinW90 <kevinjwilliams76@gmail.com>
2020-08-27 13:33:12 -0400

    fetch request displays name of pokemon on page

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ pokemon endpoint: /pokemon/{id or name}
   sprite img .sprites{}
   type .types[]
   ?description
+  ?atk, ?def, ?spd
 
 form
   one input [name or id]
@@ -18,4 +19,34 @@ card
   name
   ?description
   type
+  ?atk, ?def, ?spd
 */
+
+/*
+FORM DATA
+event listener for form submission
+  send the user data in a request
+*/
+const ROOT_URL = 'https://pokeapi.co/api/v2';
+const PKMN = '/pokemon/';
+
+//function to display pokemon data
+displayPKMN = (pkmn) => {
+  $('main').html(pkmn.name);
+}
+
+//function to fetch pokemon data
+fetchPKMN = (pkmn) => {
+  fetch(`${ROOT_URL}${PKMN}${pkmn}`)
+  .then(res => res.json())
+  .then(resJson => {console.log(resJson); displayPKMN(resJson)})
+}
+
+$(() => {
+  $('form').on('submit', e => {
+    e.preventDefault();
+    const userData = $('[name=data]').val()
+    //fetch the pokemon data
+    fetchPKMN(userData);    
+  })
+})
